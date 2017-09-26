176d16cf284485a5384bb3294f28050b94861bc3
lyang9 <lyang9@buffalo.edu>
2017-09-25 21:05:29 -0400

    First commit to the repository.

--- flaskr.py
+++ flaskr.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, request, url_for
 
 app = Flask(__name__)
 
@@ -7,6 +7,44 @@ app = Flask(__name__)
 def profile(name):
     return render_template("profile.html", name=name)
 
+@app.route('/home/')
+def home():
+    return render_template("home.html")
+
+@app.route('/dashboard/')
+def dashboard():
+    return("hi")
+    #return render_template("home.html")
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html")
+
+@app.errorhandler(405)
+def method_not_found(e):
+    return render_template("405.html")
+
+@app.route('/login/', methods=["GET","POST"])
+def login_page():
+    error = None
+    try:
+        if request.method == "POST":
+            attempted_username = request.form['username']
+            attempted_password = request.form['password']
+
+            flash(attempted_username)
+            flash(attempted_password)
+
+            if attempted_username == "admin" and attempted_password == "password":
+                return redirect(url_for('home'))
+            else:
+                error = "Invalid credentials. Try Again."
+
+        return render_template("login.html", error = error)
+
+    except Exception as e:
+        flash(e)
+        return render_template("login.html", error = error)
 
 if __name__ == '__main__':
     app.run()
